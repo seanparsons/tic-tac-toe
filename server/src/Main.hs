bbc29d009ccfbad3cc9211d46c31ce715a469e8d
Sean Parsons <git@futurenotfound.com>
2018-06-18 19:16:15 +0100

    Some basic CSS is in.

--- server/src/Main.hs
+++ server/src/Main.hs
@@ -4,6 +4,7 @@
 
 module Main where
 
+import Control.Lens
 import Control.Monad.Reader
 import Control.Monad.Reader.Wiring
 import Clay hiding ((!), empty)
@@ -21,6 +22,8 @@ import System.Environment
 import Text.Blaze.Html5 ((!))
 import qualified Text.Blaze.Html5 as H
 import qualified Text.Blaze.Html5.Attributes as HA
+import qualified Text.Blaze.Svg11 as S
+import qualified Text.Blaze.Svg11.Attributes as SA
 import Text.Read hiding (lift)
 import qualified Data.UUID as UUID
 import qualified Data.UUID.V4 as UUID
@@ -46,9 +49,24 @@ server = homepage :<|> newGamePage :<|> gamePage
 
 siteCSS :: Css
 siteCSS = do
+  ".grid" ? do
+    display $ flex
+    flexDirection $ column
+  ".grid-row" ? do
+    display $ flex
+    flexDirection $ row
   ".grid-square" ? do
-    width $ px 200
-    height $ px 200
+    width $ px 250
+    height $ px 250
+    fontSize $ px 180
+  ".square-content" ? do
+    display $ inlineBlock
+    top $ px 50
+    left $ px 50
+    marginTop $ px 0
+    marginBottom $ px 0
+    width $ px 150
+    height $ px 150
 
 getGames :: AppM GamesMap
 getGames = do
@@ -58,21 +76,40 @@ getGames = do
 modifyGamesMap :: (GamesMap -> GamesMap) -> AppM ()
 modifyGamesMap transform = do
   ref <- wiredAsk
-  liftIO $ modifyIORef ref transform
+  liftIO $ atomicModifyIORef' ref (\m -> (transform m, ()))
+
+renderNoughtOrCross :: Maybe NoughtOrCross -> H.Html
+renderNoughtOrCross Nothing       = mempty
+renderNoughtOrCross (Just Nought) = S.svg ! HA.class_ "square-content nought-square" $ do
+  S.g $ do
+    S.circle ! SA.cx "75" ! SA.cy "75" ! SA.r "65" ! SA.strokeWidth "10" ! SA.stroke "black" ! SA.fill "white"
+renderNoughtOrCross (Just Cross)  = S.svg ! HA.class_ "square-content cross-square" $ do
+  let rectangle = S.rect ! SA.x "0" ! SA.y "70" ! SA.width "150" ! SA.height "10" ! SA.fill "black"
+  S.g ! SA.transform "rotate(45 75 75)" $ rectangle
+  S.g ! SA.transform "rotate(135 75 75)" $ rectangle
+
+renderPageContents :: H.Html -> H.Html
+renderPageContents contents = H.docTypeHtml $ do
+  H.head $ do
+    H.title "Tic-Tac-Toe"
+  H.body $ do
+    H.h1 "Tic-Tac-Toe"
+    contents
+    H.style ! HA.type_ "text/css" $ do
+      H.toMarkup $ render siteCSS
 
 renderGame :: Game -> H.Html
-renderGame game = do
-  H.table $ do
+renderGame game = renderPageContents $ do
+  H.div ! HA.class_ "grid" $ do
     forM_ [1..gameSize] $ \row -> do
-      H.tr ! HA.class_ "grid-row" $ do
+      H.div ! HA.class_ "grid-row" $ do
         forM_ [1..gameSize] $ \column -> do
           let elemName = H.stringValue ("square-" ++ (show row) ++ "-" ++ (show column))
-          H.td ! HA.id elemName ! HA.class_ "grid-square" $ do
-            H.toMarkup $ show (row, column)
-
+          H.div ! HA.id elemName ! HA.class_ "grid-square" $ do
+            renderNoughtOrCross (game ^. cells . at (row, column))
 
 homepage :: AppM Homepage
-homepage = return $ H.docTypeHtml $ do
+homepage = return $ renderPageContents $ do
   H.head $ do
     H.title "Tic-Tac-Toe"
   H.body $ do
@@ -83,6 +120,7 @@ homepage = return $ H.docTypeHtml $ do
 gamePage :: T.Text -> AppM H.Html
 gamePage gameID = do
   games <- getGames
+  liftIO $ print $ show $ keys games
   let possibleGame = lookup gameID games
   case possibleGame of
     Nothing -> throwError err404
@@ -108,4 +146,10 @@ main = do
   port <- fmap (fromMaybe 8000 . (>>= readMaybe)) (lookupEnv "PORT")
   putStrLn $ "http://localhost:" ++ show port ++ "/"
   gamesRef <- newIORef empty
+  atomicModifyIORef' gamesRef (\m -> (set (at "cake") (Just newGame) m, ()))
+  atomicModifyIORef' gamesRef (\m -> (set (at "cake" . _Just . cells . at (1, 1)) (Just Nought) m, ()))
+  atomicModifyIORef' gamesRef (\m -> (set (at "cake" . _Just . cells . at (1, 2)) (Just Cross) m, ()))
+  atomicModifyIORef' gamesRef (\m -> (set (at "cake" . _Just . cells . at (1, 3)) (Just Nought) m, ()))
+  games <- readIORef gamesRef
+  print $ show games
   Warp.run port $ app gamesRef
